7d0559fcf1ab4113bb0c4d2d48ba1859a25c4154
GJayGJ <jay.gj.huang@gmail.com>
2024-07-16 13:44:41 +0100

    [Feature] Complete Unit Test for NewsListViewModel

--- ios-qa-tech-assignment/ReachNews/Mock/MockNewsAPIService.swift
+++ ios-qa-tech-assignment/ReachNews/Mock/MockNewsAPIService.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+enum MockNewsAPIServiceError: Error {
+    case mockFileNameNotSet
+    case fileURLCreationFailed(String)
+    case dataDecodingFailed(Error)
+}
+
 class MockNewsAPIService: NewsAPIService {
     var mockFileName: String?
     
@@ -17,11 +23,24 @@ class MockNewsAPIService: NewsAPIService {
     
     func getNewsData() async throws -> [Article] {
         if mockFileName == nil {
+            // Mock files can be fetched either by a given file name or by an environment key-value.
             mockFileName = ProcessInfo.processInfo.environment["MOCK_NEWS"]
         }
-        let fileURL = Bundle.main.url(forResource: mockFileName, withExtension: "json")!
-        let data = try! Data(contentsOf: fileURL)
-        let newsServiceOutput = try JSONDecoder().decode(NewsServiceOutput.self, from: data)
-        return newsServiceOutput.articles
+        
+        guard let fileName = mockFileName else {
+            throw MockNewsAPIServiceError.mockFileNameNotSet
+        }
+        
+        guard let fileURL = Bundle.main.url(forResource: fileName, withExtension: "json") else {
+            throw MockNewsAPIServiceError.fileURLCreationFailed("\(fileName).json")
+        }
+        
+        do {
+            let data = try Data(contentsOf: fileURL)
+            let newsServiceOutput = try JSONDecoder().decode(NewsServiceOutput.self, from: data)
+            return newsServiceOutput.articles
+        } catch let error {
+            throw MockNewsAPIServiceError.dataDecodingFailed(error)
+        }
     }
 }
--- ios-qa-tech-assignment/ReachNewsTests/NewsListTests.swift
+++ ios-qa-tech-assignment/ReachNewsTests/NewsListTests.swift
@@ -21,20 +21,35 @@ final class NewsListTests: XCTestCase {
         mockNewsAPIService = nil
     }
     
+    
+    // MARK: Unit Test Cases
+
     /// Test that news can be retrieved successfully
-    func testGetNews() async throws {
-        // Arrange - Dependency Injection
-        mockNewsAPIService = BitbucketNewsAPIService()
-        InjectedValues[\.newsAPIService] = mockNewsAPIService
+    func testGetNewsSuccess() async throws {
+        // Arrange
+        setupMockService()
         
-        let expectation = self.expectation(description: "News data fetch successful")
+        // Act
+        await fetchNewsData(expectationDescription: "News data fetch successful")
+        
+        // Assert
+        XCTAssertGreaterThanOrEqual(viewModel.numberOfRows(), 0)
+    }
+    
+    /// Test that fetching news is failed
+    func testGetNewsFailure() async throws {
+        // Arrange
+        setupMockService(fileName: "notExistingData")
+        
+        let expectation = self.expectation(description: "News data fetch failure")
         var isExpectationFulfilled = false
         
         // Expectation should be fulfilled once when completion handler is excuted
-        viewModel.updateUIBind = {
+        viewModel.newsErrorBind = { errorMessage in
             if !isExpectationFulfilled {
                 isExpectationFulfilled = true
                 expectation.fulfill()
+                XCTAssertNotNil(errorMessage)
             }
         }
         
@@ -43,6 +58,76 @@ final class NewsListTests: XCTestCase {
         await fulfillment(of: [expectation], timeout: 5)
         
         // Assert
-        XCTAssertGreaterThanOrEqual(viewModel.numberOfRows(), 0)
+        XCTAssertEqual(viewModel.numberOfRows(), 0)
+    }
+    
+    /// Test that number of rows matches number of articles
+    func testNumberOfRows() async throws {
+        // Arrange
+        setupMockService(fileName: "MockTwentyNewsData")
+        
+        // Act
+        await fetchNewsData(expectationDescription: "News data fetch successful")
+        
+        // Assert
+        XCTAssertEqual(viewModel.numberOfRows(), 20)
+    }
+    
+    /// Test that cellViewModel is created correctly
+    func testCellViewModel() async throws {
+        // Arrange
+        setupMockService(fileName: "MockTwentyNewsData")
+        
+        // Act
+        await fetchNewsData(expectationDescription: "News data fetch successful")
+        
+        // Assert - Verify the cellViewModel for a valid index path
+        let indexPath = IndexPath(row: 0, section: 0)
+        let cellViewModel = viewModel.cellViewModel(at: indexPath)
+        XCTAssertNotNil(cellViewModel)
+    }
+    
+    /// Test that newsDetailsViewModel is created correctly
+    func testNewsDetailsViewModel() async throws {
+        // Arrange
+        setupMockService(fileName: "MockTwentyNewsData")
+        
+        // Act
+        await fetchNewsData(expectationDescription: "News data fetch successful")
+        
+        // Assert - Verify the newsDetailsViewModel for a valid index path
+        let indexPath = IndexPath(row: 0, section: 0)
+        let newsDetailsViewModel = viewModel.newsDetailsViewModel(at: indexPath)
+        XCTAssertNotNil(newsDetailsViewModel)
+    }
+    
+    
+    // MARK: private functions
+    
+    /// Arrange - Dependency Injection
+    private func setupMockService(fileName: String? = nil) {
+        if let fileName = fileName {
+            mockNewsAPIService = MockNewsAPIService(fileName: fileName)
+        } else {
+            mockNewsAPIService = BitbucketNewsAPIService()
+        }
+        InjectedValues[\.newsAPIService] = mockNewsAPIService
+    }
+    
+    /// For the test cases that fetch news data successfully
+    private func fetchNewsData(expectationDescription: String) async {
+        let expectation = self.expectation(description: expectationDescription)
+        var isExpectationFulfilled = false
+        
+        // Expectation should be fulfilled once when completion handler is executed
+        viewModel.updateUIBind = {
+            if !isExpectationFulfilled {
+                isExpectationFulfilled = true
+                expectation.fulfill()
+            }
+        }
+        
+        viewModel.getNews()
+        await fulfillment(of: [expectation], timeout: 5)
     }
 }
